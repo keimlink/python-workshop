6f4ef725ce348b0ba075a1da3541de43f4752dff
Markus Zapke-Gründemann <markuszapke@gmx.net>
2010-03-01 11:35:23 +0100

    Improved examples and added string function examples.

--- python-workshop/f_data_structures/a_strings.py
+++ python-workshop/f_data_structures/a_strings.py
@@ -2,17 +2,35 @@
 """Strings
 """
 print __doc__
-print 'L' 'o' 's' '!'
+print 'H' 'e' 'l' 'l' 'o' '!'
 print 'Hello', 'world!'
+print 'Hello' + ' ' + 'world!'
 print """This sentence
 has multiple
-sentences."""
+lines."""
 text = "Hello\nworld."
 print text
 text_raw = r"Hello\nworld."
 print text_raw
 print '-' * 40
-print 'Hi %s' % 'Guido'
+print 'Hi %s!' % 'Guido'
 print 'Hi %s! My name is %s.' % ('Guido', 'Dennis')
+print 'Hi %(me)s! My name is %(friend)s.' % {'friend': 'Dennis', 'me': 'Guido'}
+print '-' * 40
+text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+print text.count('dolor')
+first_sentence = text[:124]
+print first_sentence
+print first_sentence.title()
+print 'istitle:', first_sentence.title().istitle()
+print 'isdigit:', first_sentence.isdigit()
+print 'isalpha:', first_sentence.isalpha()
+print 'startswith:', first_sentence.startswith('Lorem')
+print 'endswith:', first_sentence.endswith('.')
+print first_sentence.lower()
+print 'islower:', first_sentence.lower().islower()
+print 'isupper:', first_sentence.isupper()
+print first_sentence.replace('ipsum', 'python')
+print '-' * 40
 print 'An Unicode character:', unicode(u'\u0153') # will print "œ"
 print str(u'\u0153') # Raises an UnicodeEncodeError
