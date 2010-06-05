c406f8946fb2d703d77310f1fc55ab61924a31d2
Markus Zapke-Gründemann <markuszapke@gmx.net>
2010-06-06 00:37:16 +0200

    Fixed a few comments.

--- python-workshop/f_data_structures/b_lists.py
+++ python-workshop/f_data_structures/b_lists.py
@@ -7,12 +7,12 @@ print colors.count('red') # 1
 print colors.index('red') # 2
 print colors[2] # 'red'
 colors.extend(['purple', 'orange'])
-print colors
+print colors # ['blue', 'yellow', 'red', 'purple', 'orange']
 colors.sort()
-print colors # Now the list is sorted.
-print ', '.join(colors)
+print colors # ['blue', 'orange', 'purple', 'red', 'yellow']
+print ', '.join(colors) # blue, orange, purple, red, yellow
 print '-' * 40
 print colors[1] # orange
-print colors[:4] # ['blue', 'yellow', 'purple', 'orange']
-print colors[2:4] # ['purple', 'orange']
-print colors[:-3] # ['blue', 'yellow']
+print colors[:4] # ['blue', 'orange', 'purple', 'red']
+print colors[2:4] # ['purple', 'red']
+print colors[:-3] # ['blue', 'orange']
